0253097b3d5e6c71229486ab705c059ec1a63b7c
vmork <vallemork@gmail.com>
2023-11-16 17:50:52 +0100

    less logging

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, request
 import json
 import random
-from datetime import datetime, timedelta
+from datetime import datetime
 import os
 import pytz
 
 PORT = os.environ.get("PORT", 5000)
 RESET_HOUR = 19
-WORD_LIST = open("./wordlist.txt").read().split("\n")
 
 def get_valid_words(combo: str, center: str):
-    words = [w for w in WORD_LIST 
+    word_list = open("./wordlist.txt").read().split("\n")
+    words = [w for w in word_list 
              if center in w 
              and all(c in combo for c in w)]
     return words
@@ -22,7 +22,6 @@ combo_center_counts = from_json(json.load(open("combo_center_counts.json", "r"))
 
 def create_game():
     combo, center = random.choice(list(combo_center_counts.keys()))
-    print(combo, center)
     valid_words = get_valid_words(combo, center)
     pangrams = [w for w in valid_words if len(set(w)) == 7]
     game = {
@@ -32,6 +31,7 @@ def create_game():
         "pangrams": pangrams,
         "date": datetime.now().date().isoformat(),
     }
+    print("creating game:", game["date"], combo, center)
     return game
 
 
@@ -47,23 +47,21 @@ def base(path):
 
 @app.route('/get_game')
 def get_new_game():
+    print("/get_game request from", request.remote_addr)
     game_is_old = False
     try:
         game = json.load(open("game.json", "r"))
         created_date = game["date"]
         now = datetime.now(tz=pytz.timezone('Europe/Stockholm'))
         current_date = now.date().isoformat()
-        print(created_date, current_date, now.hour, RESET_HOUR)
         if (created_date != current_date) and (now.hour >= RESET_HOUR):
             game_is_old = True
         else:
-            print("Using existing game for", created_date)
             return game
     except FileNotFoundError:
         game_is_old = True
 
     if game_is_old:
-        print("Creating new game for", datetime.now().date())
         game = create_game()
         json.dump(game, open("game.json", "w"), ensure_ascii=False)
         return game
